b5f6cf12b18ecc5e6db9cdd9b1ab27c4db44175e
Tanya <tearfulta@gmail.com>
2020-11-05 15:00:00 +0300

    added middleware settings and basic layout

---
+++ middleware/index.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+  const express = require('express');
+  const path = require('path');
+  const fetch = require('node-fetch');
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+  app.set('view engine', 'hbs');
+  app.set('views', path.join(__dirname, '..', 'views'));
+};
